f69ff179502ac49c8bf28dcad966f4f4f05391fc
MARCELO MOMBERGER HAUBERT <05839895008@senacrs.edu.br>
2026-02-24 19:12:59 -0300

    Ajuste do arquivo categoria_dao

---
+++ model/dao/categoria_dao.py
@@ -0,0 +1,71 @@
+from model.categoria import Categoria
+from model.dao.base_dao import BaseDAO
+
+
+class Categoria_DAO(BaseDAO): 
+    def save(self, categoria:Categoria):
+        sql = """insert into categoria (nome)
+                values (%s)"""
+        
+        values = (categoria._nome)
+
+        conn = self._get_connection()
+        cursor = conn.cursor()
+        cursor.execute(sql, values)
+        categoria._id = cursor.lastrowid
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return categoria
+    
+    def get_all(self):
+        sql = "select id, nome from categoria"
+        conn = self._get_connection()
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        categoria = []
+        
+        for(id, nome) in  cursor:
+            categoria.append(Categoria(id, nome))
+        cursor.close()
+        conn.close()
+        return categoria 
+    
+    def get_by_id(self, id):
+        sql = """select id, nome from categoria
+                where id = %s"""
+        conn = self._get_connection()
+        cursor = conn.cursor()
+        cursor.execute(sql, (id,))
+        row = cursor.fetchone()
+        produto = None
+        if row:
+            id, nome = row
+            categoria = Categoria(id, nome)
+        cursor.close()
+        conn.close()
+        return categoria
+    
+    def delete(self, id):
+        sql = "delete from categoria where id = %s"
+        conn = self._get_connection()
+        cursor = conn.cursor()
+        cursor.execute(sql, (id,))
+        conn.commit()
+        affected_rows = cursor.rowcount
+        cursor.close()
+        conn.close()
+        return affected_rows > 0
+
+    def update(self, categoria_atualizado:Categoria):
+        sql = """update categoria set nome = %s
+                where id = %s"""
+        values = (categoria_atualizado._nome, categoria_atualizado._id)
+        conn = self._get_connection()
+        cursor = conn.cursor()
+        cursor.execute(sql, values)
+        conn.commit()
+        affected_rows = cursor.rowcount
+        cursor.close()
+        conn.close()
+        return affected_rows > 0
